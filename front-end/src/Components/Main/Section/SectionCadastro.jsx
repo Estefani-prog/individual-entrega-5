42cc6e44be46dbb6280b22881e9d5b4ee68c06d8
Estefani-prog <fannemonteiro95@gmail.com>
2022-03-24 15:30:21 -0300

    Atualização Cadastro

--- front-end/src/Components/Main/Section/SectionCadastro.jsx
+++ front-end/src/Components/Main/Section/SectionCadastro.jsx
@@ -1,11 +1,28 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './SectionCadastro.css'
+import api from '../../../api'
 
-const Formulario = () => {
+const Formulario = () => { 
+    const nome= useRef()
+    const nome= useRef()
+    const nome= useRef()
+    const nome= useRef()
     const [Nome, setNome] = useState("")
     const [CPF, setCpf] = useState("")
     const [Email, setEmail] = useState("")
     const [Endereco, setEndereco] = useState("")
+    
+    function enviarDados(event){
+        event.preventDefault()
+        api.post("/cadastro/",{
+            nome: nome.current.value,
+            nome: nome.current.value,
+            nome: nome.current.value,
+            nome: nome.current.value,
+        }).then((res)=>console.log(res.data)).catch((err)=>console.log(err))
+    }
+
+
     return (
         <section className="d-flex justify-content-center">
             <div className="card col-sm-6 p-3">
@@ -13,23 +30,23 @@ const Formulario = () => {
                     <h4>Cadastro</h4>
                 </div>
                 <div className="mb-2"></div>
-                <form action="">
+                <form action="" onSubmit={enviarDados}>
                     <div className="mb-2">
                         <label for="nome">Nome:</label>
-                        <input type="text" required className="form-control bg-light btn-outline-secondary" placeholder="Nome" value={Nome} onChange={e => setNome(e.target.value)} />
+                        <input type="text" required className="form-control bg-light btn-outline-secondary" ref={nome} placeholder="Nome" value={Nome} onChange={e => setNome(e.target.value)} />
                     </div>
                     <div className="mb-2">
                         <div className="mb-2">
                             <label for="nome">CPF:</label>
-                            <input type="text" required className="form-control bg-light btn-outline-secondary" placeholder="CPF" value={CPF} onChange={e => setCpf(e.target.value)} />
+                            <input type="text" required className="form-control bg-light btn-outline-secondary" ref={nome} placeholder="CPF" value={CPF} onChange={e => setCpf(e.target.value)} />
                         </div>
                         <label for="nome">Email:</label>
-                        <input type="text" required className="form-control bg-light btn-outline-secondary" placeholder="Email" value={Email} onChange={e => setEmail(e.target.value)} />
+                        <input type="text" required className="form-control bg-light btn-outline-secondary" ref={nome}placeholder="Email" value={Email} onChange={e => setEmail(e.target.value)} />
                     </div>
 
                     <div className="mb-2">
                         <label for="nome">Endereço:</label>
-                        <input type="text" required className="form-control bg-light btn-outline-secondary" placeholder="Estrada Matue, 56" Copacabana-RJ value={Endereco} onChange={e => setEndereco(e.target.value)} />
+                        <input type="text" required className="form-control bg-light btn-outline-secondary" ref={nome} placeholder="Estrada Matue, 56" Copacabana-RJ value={Endereco} onChange={e => setEndereco(e.target.value)} />
                     </div>
                     <div className="mb-2">
                         <br></br>
